c13cdf95371ce4f581960375137ec3d47a8cf2b0
Mohamed Kamal <M.Kamal.aly1988@gmail.com>
2023-07-05 13:19:12 +0300

    Add files via upload

--- alembic/versions/001_initial_migration.py
+++ alembic/versions/001_initial_migration.py
@@ -1,1 +1,23 @@
+from alembic import op
+import sqlalchemy as sa
 
+
+# Declare the revision variable
+revision = '001'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'videos',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('title', sa.String(100), index=True),
+        sa.Column('description', sa.String(500)),
+        sa.Column('duration', sa.Integer)
+    )
+
+
+def downgrade():
+    op.drop_table('videos')
